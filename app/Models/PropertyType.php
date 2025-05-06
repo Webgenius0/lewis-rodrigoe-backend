e08f3bef5a824c08dc52edc61cf4ea5b24b09be3
mdiktushar <md.tushar1804@gmail.com>
2025-05-06 15:53:35 +0600

    model fo property type

--- app/Models/PropertyType.php
+++ app/Models/PropertyType.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class PropertyType extends Model
 {
-    //
+    /**
+     * fillable
+     * @var array
+     */
+    protected $fillable = ['name', 'slug'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id'         => 'integer',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
 }
