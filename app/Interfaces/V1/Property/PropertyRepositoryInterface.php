04c607c0bcfc6ca939bb58f2dbcde1ad526c57b5
mdiktushar <md.tushar1804@gmail.com>
2025-05-08 17:38:27 +0600

    removing unwanted files

---
+++ app/Interfaces/V1/Property/PropertyRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces\V1\Property;
+
+interface PropertyRepositoryInterface
+{
+    // Define the methods your repository should implement
+}
---
+++ app/Repositories/V1/Property/PropertyRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\V1\Property;
+
+use App\Interfaces\V1\Property\PropertyRepositoryInterface;
+
+class PropertyRepository implements PropertyRepositoryInterface
+{
+    // Your Repository logic goes here
+}
