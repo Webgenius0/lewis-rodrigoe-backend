34e10bf6873ceccd19adf92f2297d9f37273e9f0
mdiktushar <md.tushar1804@gmail.com>
2025-05-14 10:58:26 +0600

    property job created

---
+++ app/Interfaces/V1/Property/Job/JobRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces\V1\Property\Job;
+
+interface JobRepositoryInterface
+{
+    // Define the methods your repository should implement
+}
---
+++ app/Repositories/V1/Property/Job/JobRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\V1\Property\Job;
+
+use App\Interfaces\V1\Property\Job\JobRepositoryInterface;
+
+class JobRepository implements JobRepositoryInterface
+{
+    // Your Repository logic goes here
+}
