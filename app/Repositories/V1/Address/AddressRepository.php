62e51b4d72ddda1b26cfa4acbbb5d03e6522dc9b
mdiktushar <md.tushar1804@gmail.com>
2025-05-21 16:43:21 +0600

    update address

--- app/Repositories/V1/Address/AddressRepository.php
+++ app/Repositories/V1/Address/AddressRepository.php
@@ -30,8 +30,33 @@ class AddressRepository implements AddressRepositoryInterface
                 'latitude'   => $data['latitude'],
                 'longitude'  => $data['longitude'],
             ]);
-        }catch(Exception $e) {
+        } catch (Exception $e) {
             Log::error('AddressRepository::getList', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateAddress
+     * @param \App\Models\Address $address
+     * @return void
+     */
+    public function updateAddress(array $data, Address $address)
+    {
+        try {
+            $address->update([
+                'label'      => $data['label'],
+                'street'     => $data['street'],
+                'apartment'  => $data['apartment'],
+                'country_id' => $data['country_id'],
+                'state_id'   => $data['state_id'],
+                'city_id'    => $data['city_id'],
+                'zip_id '    => $data['zip_id'],
+                'latitude'   => $data['latitude'],
+                'longitude'  => $data['longitude'],
+            ]);
+        } catch (Exception $e) {
+            Log::error('AddressRepository::updateAddress', ['error' => $e->getMessage()]);
             throw $e;
         }
     }
