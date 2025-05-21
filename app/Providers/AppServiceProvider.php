17d34b11063e4c570e3711f337b7462ec2174836
mdiktushar <md.tushar1804@gmail.com>
2025-05-21 11:07:14 +0600

    repo bidn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,13 @@ use App\Interfaces\V1\Auth\ForgetPasswordRepositoryInterface;
 use App\Interfaces\V1\Auth\OTPRepositoryInterface;
 use App\Interfaces\V1\Auth\PasswordRepositoryInterface;
 use App\Interfaces\V1\Auth\UserRepositoryInterface;
+use App\Interfaces\V1\BankAccount\BankAccountRepositoryInterface;
 use App\Interfaces\V1\Boiler\Model\BoilerModelRepositoryInterface;
 use App\Interfaces\V1\Boiler\Type\BoilerTypeRepositoryInterface;
 use App\Interfaces\V1\Engineer\EngineerRepositoryInterface;
 use App\Interfaces\V1\GassSafetyRegistration\GassSafetyRegistrationRepositoryInterface;
 use App\Interfaces\V1\NICEIC\NICEICRepositoryInterface;
+use App\Interfaces\V1\NQR\NVQRepositoryInterface;
 use App\Interfaces\V1\Property\Job\PropertyJobRepositoryInterface;
 use App\Interfaces\V1\Property\PropertyRepositoryInterface;
 use App\Interfaces\V1\Property\Type\PropertyTypeRepositoryInterface;
@@ -30,11 +32,13 @@ use App\Repositories\V1\Auth\ForgetPasswordRepository;
 use App\Repositories\V1\Auth\OTPRepository;
 use App\Repositories\V1\Auth\PasswordRepository;
 use App\Repositories\V1\Auth\UserRepository;
+use App\Repositories\V1\BankAccount\BankAccountRepository;
 use App\Repositories\V1\Boiler\Model\BoilerModelRepository;
 use App\Repositories\V1\Boiler\Type\BoilerTypeRepository;
 use App\Repositories\V1\Engineer\EngineerRepository;
 use App\Repositories\V1\GassSafetyRegistration\GassSafetyRegistrationRepository;
 use App\Repositories\V1\NICEIC\NICEICRepository;
+use App\Repositories\V1\NQR\NVQRepository;
 use App\Repositories\V1\Property\Job\PropertyJobRepository;
 use App\Repositories\V1\Property\PropertyRepository;
 use App\Repositories\V1\Property\Type\PropertyTypeRepository;
@@ -88,6 +92,11 @@ class AppServiceProvider extends ServiceProvider
         // NICEIC
         $this->app->bind(NICEICRepositoryInterface::class, NICEICRepository::class);
 
+        // NVQ
+        $this->app->bind(NVQRepositoryInterface::class, NVQRepository::class);
+
+        // Bank accoutn
+        $this->app->bind(BankAccountRepositoryInterface::class, BankAccountRepository::class);
     }
 
     /**
