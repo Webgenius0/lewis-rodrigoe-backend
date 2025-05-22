1c1b2face875038fd92f0c1fe6228ead5d9e9614
mdiktushar <md.tushar1804@gmail.com>
2025-05-22 12:55:11 +0600

    user delete

--- routes/api/v1/user/auth.php
+++ routes/api/v1/user/auth.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/auth-user')->controller(AuthProfileController::class)->group(function () {
     Route::get('/', 'show');
     Route::post('/','update');
+    Route::delete('/', 'destory');
 });
