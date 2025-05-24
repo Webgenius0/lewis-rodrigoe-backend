b2d6d7b91034e0c1ad20b51e1518e2641be456d4
mdiktushar <md.tushar1804@gmail.com>
2025-05-24 15:00:29 +0600

    request created

---
+++ app/Http/Requests/Api/V1/Message/SendRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Message;
+
+use App\Traits\V1\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class SendRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'receiver_id' => 'required|exists:users,id',
+            'content'     => 'required|string',
+        ];
+    }
+
+    /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $fieldsToCheck = [
+            'receiver_id',
+            'content',
+        ];
+        $message = 'Validation error';
+
+        foreach ($fieldsToCheck as $field) {
+            $errors = $validator->errors()->get($field);
+            if (!empty($errors)) {
+                $message = $errors[0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+
+        throw new ValidationException($validator, $response);
+    }
+}
