362364d3000e2e855e21d6a0d842dbd718eccd73
mdiktushar <md.tushar1804@gmail.com>
2025-05-26 16:57:31 +0600

    onlien hour get

--- app/Interfaces/V1/OnlineHoiur/OnlineHourRepositoryInterface.php
+++ app/Interfaces/V1/OnlineHoiur/OnlineHourRepositoryInterface.php
@@ -2,7 +2,14 @@
 
 namespace App\Interfaces\V1\OnlineHoiur;
 
+use App\Models\OnlineHour;
+use Illuminate\Database\Eloquent\Collection;
+
 interface OnlineHourRepositoryInterface
 {
-    // Define the methods your repository should implement
+    /**
+     * getOnlineHours
+     * @return Collection<int, OnlineHour>
+     */
+    public function getOnlineHours(): Collection;
 }
--- app/Repositories/V1/OnlineHoiur/OnlineHourRepository.php
+++ app/Repositories/V1/OnlineHoiur/OnlineHourRepository.php
@@ -3,8 +3,17 @@
 namespace App\Repositories\V1\OnlineHoiur;
 
 use App\Interfaces\V1\OnlineHoiur\OnlineHourRepositoryInterface;
+use App\Models\OnlineHour;
+use Illuminate\Database\Eloquent\Collection;
 
 class OnlineHourRepository implements OnlineHourRepositoryInterface
 {
-    // Your Repository logic goes here
+    /**
+     * getOnlineHours
+     * @return Collection<int, OnlineHour>
+     */
+    public function getOnlineHours():Collection
+    {
+        return OnlineHour::all();
+    }
 }
